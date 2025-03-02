8ffa4c507adcffd2547d2aab0e649c156e0127d6
John Leonard <john_t_leonard@apple.com>
2025-03-02 12:58:26 -0600

    checkpoint

--- cmake-build-debug/CMakeFiles/fc-tmp/sfml/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for sfml")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/Users/johnleonard/CLionProjects/CppTimber/cmake-build-debug/CMakeFiles/fc-tmp/sfml/sfml-gitupdate.cmake")
-
-endblock()
--- cmake-build-debug/_deps/flac-build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/johnleonard/CLionProjects/Timber/cmake-build-debug/_deps/flac-src/src
-# Build directory: /Users/johnleonard/CLionProjects/Timber/cmake-build-debug/_deps/flac-build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libFLAC")
